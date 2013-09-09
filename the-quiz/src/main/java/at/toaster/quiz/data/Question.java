de391bf100b6cd1e3342b5fd60da2e4b7de0f4cc
Rene8888 <rene.hollander@hotmail.de>
2013-09-09 13:09:06 +0200

    Fixed that questionText in Question was not set

--- the-quiz/src/main/java/at/toaster/quiz/data/Question.java
+++ the-quiz/src/main/java/at/toaster/quiz/data/Question.java
@@ -16,6 +16,8 @@ public class Question implements Serializable {
 	public Question(String questionText, Answer answer1, Answer answer2,
 			Answer answer3, Answer answer4) {
 		
+		this.questionText = questionText;
+		
 		this.answer1 = answer1;
 		this.answer2 = answer2;
 		this.answer3 = answer3;
